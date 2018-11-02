c14637e707225ef53ff5f7e7a75e294829372666
kouchao <admin@jskou.com>
2018-11-02 11:56:17 +0800

    feat: add randomStr

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 const mongoHost = 'mongodb://xxx'
 const mongoPort = 'xxx'
 const mongoDatabase = 'xxx'
+const randomStr = 'f06f3fb0de5011e89f32f2801f1b9fd1'
+
 module.exports = {
 	mongoHost,
 	mongoPort,
-	mongoDatabase
+	mongoDatabase,
+	randomStr
 }
 ```
--- api/list.js
+++ api/list.js
@@ -1,4 +1,5 @@
 const {list, visit} = require('../db');
+const { randomStr } = require('../config.js');
 
 function countAdd() {
 	visit.create({
@@ -23,20 +24,28 @@ const getLinks = async (ctx) => {
 }
 
 const addLink = async (ctx) => {
-	let {title, link} = ctx.request.body
+	let {title, link, random} = ctx.request.body
 
-	try {
-		await list.create({
-			title,
-			link
-		});
+	if(randomStr == random){
+		try {
+			await list.create({
+				title,
+				link
+			});
 
-		ctx.body = {
-			code: 0
+			ctx.body = {
+				code: 0
+			}
+		} catch (err) {
+			ctx.body = err
 		}
-	} catch (err) {
-		ctx.body = err
+	} else {
+		ctx.body = {
+			code: 1,
+			msg: '随机串不匹配'
+		}
 	}
+
 }
 
 
