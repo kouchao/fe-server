083da9596914db45023b364c84e1d1b47405caa1
kouchao <admin@jskou.com>
2018-12-07 08:46:04 +0800

    fix: 修复 分页能够获取邮箱数据的bug

--- api/message.js
+++ api/message.js
@@ -4,7 +4,7 @@ const list = async (ctx) => {
 	try {
 		let {page, pageSize} = ctx.request.query
 
-		const data = await message.find()
+		const data = await message.find({},{name: 1, content: 1})
 		.sort({time: -1})
 		.skip(page * pageSize || 0)
 		.limit(pageSize * 1 || 30)
