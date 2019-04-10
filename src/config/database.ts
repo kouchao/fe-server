ffefae8fb000fcf27296b7dde9129db294e98721
kouchao <admin@jskou.com>
2019-04-10 14:33:56 +0800

    feat: 新增 数据加密

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,15 @@
 import * as mongoose from 'mongoose';
-import {mongoHost, mongoPort, mongoDatabase} from './config';
-
-mongoose.connect(`${mongoHost}:${mongoPort}/${mongoDatabase}`);
-
+import {mongoHost, mongoPort, mongoDatabase, mongoUsername, mongoPassword} from './config';
+const url = `mongodb://${mongoHost}:${mongoPort}/${mongoDatabase}`
+mongoose.connect(url, {
+	useNewUrlParser: true,
+	auth: {
+		authSource: 'admin',
+		user: mongoUsername,
+		password: mongoPassword
+	}
+	
+});
 
 mongoose.connection.on('error', (err) => {
 	console.log('connect error:', err)
