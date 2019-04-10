a077564967e75ad0f24957dd1501e12f8b712cbd
kouchao <admin@jskou.com>
2019-04-10 14:40:25 +0800

    feat: 新增 钉钉hooks'

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,11 +1,71 @@
 import {List, Visit} from '../models';
-const { randomStr } = require('../config/config.js');
+import axios from 'axios';
+import { randomStr, dingding } from '../config/config.js';
 
+// 添加访问数据
 function countAdd(type) {
 	Visit.create({
 		type
 	})
 }
+
+// 处理数据
+function getList(msg, time, type) {
+	let tag = ''
+	msg = msg.replace(/\t+/g,'').replace(/\n\n+/g,'\n').split('\n')
+			
+	msg = msg.map(o => {
+	tag = o.match(/\[(\S*)\]/)&&o.match(/\[(\S*)\]/)[1] || '';
+		let arr = o.replace(`[${tag}]`,'').split('http')
+		return arr[0] && arr[1] ? {
+			title: arr[0],
+			link: 'http' + arr[1],
+			type,
+			time,
+			tag
+		} : {
+			title: '记录失败！请检查',
+			link: '/',
+			type,
+			time,
+			tag
+		}
+	}).filter(o => o)
+	return msg
+}
+
+
+// 发送钉钉
+function sendDingDing(lists){
+	try {
+		let md = {
+			msgtype: 'markdown',
+			markdown: {
+				text: '### 每日早报 \n'
+			},
+			at: {
+				isAtAll: false
+			}
+		}
+		
+		lists.forEach(o => {
+			md.markdown.text += `- [${o.title}](${o.link}) \n`
+		})
+		md.markdown.text += '> [查看更多历史](http://fe.jskou.com) \n'
+
+		dingding.forEach(token => {
+			const url = `https://oapi.dingtalk.com/robot/send?access_token=${token}`
+			axios.post(url, md)
+		})
+	
+		
+	}catch(err) {
+		console.log('err')
+		console.log(err)
+	}
+
+}
+
 
 export const getLinks = async (ctx) => {
 	try {
@@ -43,7 +103,7 @@ export const getLinks = async (ctx) => {
 }
 
 export const addLink = async (ctx) => {
-	let {title, link, type, random, tag, time} = ctx.request.body
+	let {msg, type, random, time} = ctx.request.body
 
 		try {
 
@@ -55,14 +115,9 @@ export const addLink = async (ctx) => {
 				throw '类型不能为空'
 			}
 
-			await List.create({
-				title,
-				type,
-				link,
-				tag,
-				time: new Date(time)
-			});
-
+			const lists = getList(msg, time, type)
+			sendDingDing(lists)
+			await List.insertMany(lists)
 			ctx.body = {
 				code: 0
 			}
